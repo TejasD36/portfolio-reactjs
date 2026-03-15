65a8674bb7c025c8546faad67ae18a7d48d8bb4f
TejasDudhal36 <tejas_dudhal@bpointer.in>
2026-03-15 18:52:13 +0530

    constants defined in single place

---
+++ src/constants/portfolioData.js
@@ -0,0 +1,43 @@
+export const portfolioData = {
+  banner: {
+    tagline: "Welcome to my Portfolio",
+    title: "Hi! I'm Tejas",
+    roles: [
+      "Flutter Developer",
+      "Mobile App Developer",
+      "Android Developer",
+      "iOS Developer",
+    ],
+    description:
+      "Mobile Application Developer specializing in Flutter with experience building scalable Android and iOS apps across fintech, healthcare, IoT, and delivery platforms.",
+  },
+
+  social: {
+    linkedin: "https://www.linkedin.com/in/tejas-dudhal",
+    github: "https://github.com/TejasD36",
+    email:
+      "https://mail.google.com/mail/?view=cm&to=tejasdudhal05@gmail.com&su=Portfolio%20Contact",
+  },
+
+  resume: {
+    url: "https://drive.google.com/file/d/143sz3WHE8xqElouifEBac-wpja83kY5p/view?usp=sharing",
+  },
+
+  footer: {
+    copyright: "Copyright 2026. All Rights Reserved",
+  },
+
+  skills: {
+    skillSummary:
+      "Experienced in building scalable mobile applications using Flutter, developing cross-platform Android & iOS apps, integrating APIs, and working with IoT Bluetooth devices for real-time data communication.",
+    skillsList: [
+      { name: "Flutter Development", level: 95 },
+      { name: "Mobile App Architecture", level: 90 },
+      { name: "REST API Integration", level: 92 },
+      { name: "Bluetooth / IoT Apps", level: 88 },
+      { name: "Firebase Integration", level: 85 },
+      { name: "UI Implementation", level: 85 },
+      { name: "Git & Version Control", level: 85 },
+    ],
+  },
+};
