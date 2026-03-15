aff105d53d99de1e8b881ffa8da6b7d523ffa6f9
TejasDudhal36 <tejas_dudhal@bpointer.in>
2026-03-15 18:53:22 +0530

    feat: Navbar & Footer component created

---
+++ src/components/Footer.js
@@ -0,0 +1,48 @@
+import { Container, Row, Col } from "react-bootstrap";
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+import { portfolioData } from "../constants/portfolioData";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <Col size={12} sm={6}>
+            <img src={logo} alt="Logo" />
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a
+                href={portfolioData.social.linkedin}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={navIcon1} alt="LinkedIn" />
+              </a>
+
+              <a
+                href={portfolioData.social.github}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={navIcon2} alt="GitHub" />
+              </a>
+
+              <a
+                href={portfolioData.social.email}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={navIcon3} alt="Email" />
+              </a>
+            </div>
+            <p>{portfolioData.footer.copyright}</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
